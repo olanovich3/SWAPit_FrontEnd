4d7cf90cb2d091b64f653100425e949eb65a248e
FabioFB99 <fabiof1999@gmail.com>
2023-03-23 14:33:08 +0100

    edit product donete

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -1,12 +1,12 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { ProductContext } from '../context/ProductContext';
 import { API } from '../services/API';
+import Palette from '../styles/Palette';
 import Button from '../ui-components/Button';
-
 const EditProductStyled = styled.main`
   display: flex;
   align-items: center;
@@ -17,11 +17,22 @@ const EditProductStyled = styled.main`
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
     padding: 20px;
     width: 60%;
-    height: 90%;
+    height: 99%;
     display: flex;
     align-items: center;
   }
-
+  & .img1 {
+    width: 100%;
+    object-fit: cover;
+  }
+  & .img2 {
+    width: 100%;
+    object-fit: cover;
+  }
+  & .img3 {
+    width: 100%;
+    object-fit: cover;
+  }
   & .form {
     display: flex;
     align-items: center;
@@ -47,15 +58,30 @@ const EditProductStyled = styled.main`
   }
 
   & .editproductimg {
-    width: 50%;
+    width: 30%;
     height: 80%;
   }
-  & .editproductimg img {
-    width: 100%;
-    height: 100%;
-  }
   & .inputdescription {
-    height: 60px;
+    height: 70px;
+  }
+  & .inputfile {
+    display: none;
+  }
+  .inputfile + label {
+    cursor: pointer;
+    width: 100%;
+  }
+  & .buttonfile {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 6px;
+    height: 21px;
+  }
+  & .buttonfile:hover {
+    padding: 8px;
+    background-color: ${Palette.secondary};
+    color: ${Palette.background};
   }
 `;
 
@@ -63,29 +89,51 @@ const EditProduct = () => {
   let navigate = useNavigate();
   const { register, handleSubmit } = useForm();
   const { editProduct } = useContext(ProductContext);
+
   const defaultsValue = {
     title: editProduct.title,
+    owner: editProduct.owner,
     image1: editProduct.image1,
+    image2: editProduct.image2,
+    image3: editProduct.image3,
     description: editProduct.description,
-    location: editProduct.location,
     category: editProduct.category,
     condition: editProduct.condition,
     status: editProduct.status,
   };
+  const [showImage2, setShowImage2] = useState(editProduct.image3);
+  const [showImage3, setShowImage3] = useState(editProduct.image2);
+  const [showImage1, setShowImage1] = useState(editProduct.image1);
+  const [valueimg, setValueimg] = useState(editProduct.image1);
+  const [valueimg2, setValueimg2] = useState(editProduct.image2);
+  const [valueimg3, setValueimg3] = useState(editProduct.image3);
+  const onChangeimg1 = (e) => {
+    setValueimg(e.target.files[0]);
+    setShowImage1(URL.createObjectURL(e.target.files[0]));
+  };
+  const onChangeimg2 = (e) => {
+    setValueimg2(e.target.files[0]);
+    setShowImage2(URL.createObjectURL(e.target.files[0]));
+  };
+  const onChangeimg3 = (e) => {
+    setValueimg3(e.target.files[0]);
+    setShowImage3(URL.createObjectURL(e.target.files[0]));
+  };
   const formSubmit = (formData) => {
-    console.log(formData);
+    /* console.log(formData); */
     const data = {
       title: formData.title,
-      image1: formData.image1 ? formData.image1[0] : null,
-      image2: formData.image2 ? formData.image2[0] : null,
-      image3: formData.image3 ? formData.image3[0] : null,
+      image1: valueimg,
+      image2: valueimg2,
+      image3: valueimg3,
       description: formData.description,
-      location: formData.location,
       category: formData.category,
       condition: formData.condition,
       status: formData.status,
     };
-    API.patch(`/${editProduct._id}`, data).then(() => {
+    API.patch(`/products/${editProduct._id}`, data, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    }).then(() => {
       navigate('/profile');
     });
   };
@@ -95,7 +143,21 @@ const EditProduct = () => {
         <form className="form" onSubmit={handleSubmit(formSubmit)}>
           <div className="editproductimgdata">
             <div className="editproductimg">
-              <img src={defaultsValue.image1} alt={defaultsValue.title} />
+              {defaultsValue.image1 != null && (
+                <>
+                  <img className="img1" src={showImage1} alt="" />
+                </>
+              )}
+              {defaultsValue.image2 != null && (
+                <>
+                  <img className="img2" src={showImage2} alt="" />
+                </>
+              )}
+              {defaultsValue.image3 != null && (
+                <>
+                  <img className="img3" src={showImage3} alt="" />
+                </>
+              )}
             </div>
 
             <div className="editproductdata">
@@ -114,6 +176,7 @@ const EditProduct = () => {
                 type="text"
                 id="description"
                 name="description"
+                defaultValue={defaultsValue.description}
                 placeholder="If you want you can put the description of your product"
                 {...register('description')}
                 required
@@ -145,23 +208,36 @@ const EditProduct = () => {
                 id="image1"
                 placeholder="image1"
                 {...register(`image1`)}
+                onChange={onChangeimg1}
               />
+              <label htmlFor="image1">
+                <nav className="buttonfile">Change the first image</nav>
+              </label>
               <input
                 type="file"
                 className="inputfile"
                 id="image2"
                 placeholder="image2"
                 {...register(`image2`)}
+                onChange={onChangeimg2}
               />
+              <label htmlFor="image2">
+                <nav className="buttonfile">Change the second image</nav>
+              </label>
               <input
                 type="file"
                 className="inputfile"
                 id="image3"
                 placeholder="image3"
                 {...register(`image3`)}
+                onChange={onChangeimg3}
               />
+              <label htmlFor="image3">
+                <nav className="buttonfile">Change the third image</nav>
+              </label>
             </div>
           </div>
+
           <Button type="submit" text="EDIT PRODUCT" />
         </form>
       </div>
