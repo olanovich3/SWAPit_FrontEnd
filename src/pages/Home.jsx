a956aaa60887fd72ee4511e977a982d234fe7dc3
meryroo <meryrooprada@gmail.com>
2023-03-23 19:38:19 +0100

    products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import './Home.css';
 
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { UserContext } from '../context/UserContext';
 import Button from '../ui-components/Button';
 import Carousel from '../ui-components/Carousel';
 import CarouselByFavorites from '../ui-components/CarouselbyFavorites';
@@ -9,16 +13,30 @@ import DivFlex from '../ui-components/DivFlex';
 import Search from '../ui-components/Search';
 
 const Home = () => {
+  const { value, setValue } = useContext(UserContext);
+  const [searchValue, setSearchValue] = useState('');
+  const navigate = useNavigate();
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setValue(searchValue);
+    navigate('/products');
+    console.log(value);
+  };
   return (
     <main className="home">
       <h1>Give what you don’t need & find what you’re missing</h1>
       <h2>from your heart to theirs, through our app</h2>
       <div className="h-search">
         <Search
+          value={searchValue}
+          action={handleInputChange}
           className={'categories'}
           placeholder={'🔍 Search in all categories'}
         ></Search>
-        <Button text={'Search'}></Button>
+        <Button action={handleSearch} text={'Search'}></Button>
       </div>
       <DivFlex direction={'column'} gap={'2rem'} padding={'3rem 0'}>
         <h2>Discover</h2>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,16 +1,90 @@
-/* import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
 
+import { UserContext } from '../context/UserContext';
 import { API } from '../services/API';
 
-const getAllProducts = () => {
+const ProductsStyled = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  padding: 4rem;
+
+  & .productscard {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 1px solid black;
+    padding: 1rem;
+    gap: 1rem;
+    width: 250px;
+    height: 320px;
+    border-radius: 0.5rem;
+  }
+  & .productscard img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    overflow: hidden;
+  }
+  & .productscard h3 {
+    font-size: 16px;
+    text-align: center;
+  }
+`;
+const Categories = () => {
+  const { value } = useContext(UserContext);
   const [products, setProducts] = useState([]);
-  API.get('/products').then((res) => {
-    const filterProducst = res.data.filter((item) => console.log(item));
-    setProducts(res.data);
-  });
+  const [load, setLoad] = useState(false);
+  load;
+  const [filterProd, setFilterProd] = useState([]);
+  const getProducts = () => {
+    API.get('/products').then((res) => {
+      setProducts(res.data);
+      setFilterProd(res.data);
+      localStorage.setItem('products', JSON.stringify(res.data));
+      filterProducts(value);
+      setLoad(true);
+      if (!value) {
+        return [];
+      }
+    });
+  };
+  const filterProducts = (value) => {
+    console.log(value);
+    const products = JSON.parse(localStorage.getItem('products'));
+    console.log(products);
+    const filterProducts = products.filter(
+      (product) =>
+        product.title.toLowerCase().includes(value.toLowerCase()) ||
+        product.category.toLowerCase().includes(value.toLowerCase()),
+    );
+
+    setFilterProd(filterProducts);
+  };
   useEffect(() => {
-    getAllProducts();
-  }, []);
-  return <main></main>;
+    getProducts();
+  }, [value]);
+
+  console.log(filterProd);
+
+  return (
+    <main>
+      <ProductsStyled>
+        {products.map((item) => {
+          return (
+            <figure key={item._id} className="productscard">
+              <img src={item.image1} alt={item.title} />
+              <h3>{item.title}</h3>
+            </figure>
+          );
+        })}
+      </ProductsStyled>
+    </main>
+  );
 };
- */
+
+export default Categories;
