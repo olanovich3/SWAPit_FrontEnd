98ce297bf54c2879faf7d6333cd51c6efdaf768e
Carlos Olano <carlos.olano@live.com>
2023-03-27 12:49:35 +0200

    carlosupdate22

--- src/pages/UserCard.jsx
+++ src/pages/UserCard.jsx
@@ -1,6 +1,10 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { UserContext } from '../context/UserContext';
+import { API } from '../services/API';
+import AverageRating from '../ui-components/AverageRating';
 import Button from '../ui-components/Button';
 import StarRatingInput from '../ui-components/StarsRatingInput';
 
@@ -12,37 +16,116 @@ const UserStyled = styled.main`
   gap: 2rem;
 
   & .usercard {
-    background-color: white;
-    border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
-    padding: 32px;
-    width: 550px;
-    height: 400px;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    gap: 1rem;
+    width: 100%;
+    max-width: 450px;
+    margin: 0 auto;
+    background-color: #f8f8f8;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+  .usercard img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    object-fit: cover;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+  }
+  .usercard > div {
+    padding: 20px;
+    width: 100%;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    background-color: white;
+  }
+  .usercard div {
+    text-align: center;
+  }
+  .usercard > div nav.username {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+  .usercard > div nav.username::before {
+    content: '';
+    position: absolute;
+    top: -1px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 80%;
+    height: 1px;
+    background-color: #ddd;
+  }
+  .usercard > div h2,
+  .usercard > div h3 {
+    margin: 0;
+    text-align: center;
+  }
+
+  .usercard > div nav.userbutton {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
   & .userbuttons {
     display: flex;
     gap: 48px;
   }
-  & .usercomment {
-    background-color: white;
+  .usercomment {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 auto;
+    background-color: #f8f8f8;
+    border: 1px solid #ddd;
     border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
-    padding: 32px;
-    width: 550px;
-    height: 400px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+  .usercomment form {
     display: flex;
-    justify-content: space-between;
     flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    padding: 20px;
+    box-sizing: border-box;
   }
 
-  & .usercard img {
-    width: 60%;
+  .usercomment label {
+    font-size: 16px;
+    margin-bottom: 10px;
   }
+
+  .usercomment input[type='text'],
+  .usercomment .star-rating-wrapper {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  .usercomment .commentbutton {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-top: 20px;
+  }
+
+  .usercomment .commentbutton button {
+    width: 48%;
+    border-radius: 5px;
+    font-size: 16px;
+  }
+
   & .username {
     display: flex;
     gap: 16px;
@@ -71,28 +154,31 @@ const UserStyled = styled.main`
 `;
 
 const UserCard = () => {
+  let navigate = useNavigate();
+  const { user } = useContext(UserContext);
   const [userCard, setUserCard] = useState(true);
-  const [comment, setComment] = useState(false);
+  const userProductStorage = JSON.parse(localStorage.getItem('product'));
   // const [chat, setChat] = useState(false);
   const [profile, setProfile] = useState(true);
   const [review, setReview] = useState(false);
-  const [rating, setRating] = useState(0);
   const resetComment = {
+    userfrom: user._id,
+    userto: userProductStorage.owner._id,
+    product: userProductStorage._id,
     comment: '',
     rating: 0,
   };
-  console.log(comment);
-  console.log(rating);
 
-  //PREGUNTAR MAÑANA SI ESTO ESTA BIEN HECHO AASI
   const [newComment, setNewComment] = useState({ ...resetComment });
   const handleSubmit = (ev) => {
     ev.preventDefault();
     console.log(newComment);
+    API.post('/user/comments/new', newComment).then(() => {
+      navigate('/products');
+    });
   };
   const handleRatingChange = (value) => {
     setNewComment({ ...newComment, rating: value });
-    setRating(value);
   };
 
   return (
@@ -119,27 +205,22 @@ const UserCard = () => {
       {profile &&
         (userCard ? (
           <div className="usercard">
-            <img
-              src="https://res.cloudinary.com/dysog0ybg/image/upload/v1677452413/FFVII/rafafff_j26z2x.webp"
-              alt=""
-            />
-            <nav className="username">
-              <h2>NAME</h2>
-              <h2>LASTNAME</h2>
-            </nav>
-            <h3>Valoracion</h3>
-            <nav className="userbutton">
-              <Button
-                type="button"
-                text="COMMENT"
-                action={() => setUserCard(false) & setComment(true)}
-              />
-              <Button
-                text="CHAT"
-                type="button"
-                // action={() => setUserCard(false) & setChat(true)}
-              />
-            </nav>
+            <img src={userProductStorage.owner.avatar} alt="" />
+            <div>
+              <nav className="username">
+                <h2>{userProductStorage.owner.name}</h2>
+                <h2>{userProductStorage.owner.lastname}</h2>
+              </nav>
+              <AverageRating ratings={userProductStorage.owner.rating} />
+              <nav className="userbutton">
+                <Button type="button" text="COMMENT" action={() => setUserCard(false)} />
+                <Button
+                  text="CHAT"
+                  type="button"
+                  // action={() => setUserCard(false) & setChat(true)}
+                />
+              </nav>
+            </div>
           </div>
         ) : (
           <div className="usercomment">
@@ -159,11 +240,7 @@ const UserCard = () => {
 
               <nav className="commentbutton">
                 <Button type="submit" text={'Submit'} />
-                <Button
-                  type="button"
-                  text="close"
-                  action={() => setComment(false) & setUserCard(true)}
-                />
+                <Button type="button" text="close" action={() => setUserCard(true)} />
               </nav>
             </form>
           </div>
