eb80c8b8103f79b971f4d49f269d7760fa851af0
meryroo <meryrooprada@gmail.com>
2023-03-29 01:10:01 +0200

    request2

--- src/ui-components/RequestModal.jsx
+++ src/ui-components/RequestModal.jsx
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import { ProductContext } from '../context/ProductContext';
 
+import { ProductContext } from '../context/ProductContext';
 import { UserContext } from '../context/UserContext';
 import { API } from '../services/API';
 
@@ -24,6 +24,7 @@ const RequestStyled = styled.div`
   }
   & .hidden {
     display: none !important;
+    background: none;
   }
 `;
 
@@ -33,6 +34,7 @@ const RequestModal = () => {
   const { register, handleSubmit } = useForm();
   const [item, setItem] = useState([]);
   item;
+
   const getProduct = () => {
     API.get(`/products/${requestID}`).then((res) => {
       console.log(res.data);
